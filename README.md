fef819fc99fbfbfb4158c5853ba8830acbc5f739
Sushmoy Roy <sushmoyr@gmail.com>
2021-12-14 18:15:31 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Organic Cosmetics App UI
+
+A ui concept design using flutter.
+
+This is not a complete application.
+
+## Screenshots
+
+**To be updated**
---
+++ android/app/src/main/kotlin/com/example/organic_cosmetics_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.organic_cosmetics_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
